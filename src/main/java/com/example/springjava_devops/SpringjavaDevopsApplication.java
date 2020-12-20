da33a57d33052faaf0f1825c6bca52a619ed7cc9
rameshkesavan <kramesh1983@gmail.com>
2020-12-20 15:23:30 +0800

    initial

---
+++ src/main/java/com/example/springjava_devops/SpringjavaDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springjava_devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjavaDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringjavaDevopsApplication.class, args);
+	}
+
+}
